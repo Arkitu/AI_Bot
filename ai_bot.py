99f3e5cbcb2a6a99e146890d69d82b669b563204
Overgame360 <maxime@tlon.fr>
2022-03-22 08:54:09 +0100

    Bot fonctionel

--- ai_bot.py
+++ ai_bot.py
@@ -1,3 +1,4 @@
+from cmath import log
 from dotenv import load_dotenv
 import discord, os, json
 from transformers import pipeline, set_seed
@@ -9,7 +10,7 @@ load_dotenv("api_keys.env")
 bot = discord.Client()
 
 #Démarre l'IA
-generator = pipeline('text-generation', model='gpt2')
+generator = pipeline('text-generation')
 set_seed(42)
 
 with open("chat_logs.json", "r") as fp:
@@ -25,12 +26,12 @@ def ask(question, message):
     user = message.author
     if channel_id not in chat_logs.keys():
         chat_logs[channel_id] = chat_logs["default_version"]
-    while len(chat_logs[channel_id]["content"]) > 2000:
-        array = chat_logs[channel_id].split("\n")
+    while len(chat_logs[channel_id]["content"]) > 500:
+        array = chat_logs[channel_id]["content"].split("\n")
         array.pop(0)
-        chat_logs[channel_id] = "\n".join(array)
+        chat_logs[channel_id]["content"] = "\n".join(array)
     prompt = f'{chat_logs[channel_id]["content"]}{user.name}: {question}\nAI:'
-    result = generator(prompt)
+    result = generator(prompt, max_length=1000)
     answer = result[0]["generated_text"][len(prompt):-1].split("AI:")[0]
     chat_logs[channel_id]["content"] = prompt + answer + '\n'
     save()
@@ -43,9 +44,10 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
-        if bot.user.mentioned_in(message) or message.channel.id == 735091182769537037:
+        if bot.user.mentioned_in(message) or message.channel.id == 954819888176828426:
             question = message.content.replace(f'<@!{str(bot.user.id)}>', "")
             answer = ask(question, message)
             await message.channel.send(answer)
+            print("Message sended")
 
 bot.run(os.environ.get('DISCORD_KEY'))
